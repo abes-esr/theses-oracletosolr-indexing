5cc9aa395c932107827f5abb929bbd03984171bb
William Donnette <donnette@abes.fr>
2025-02-17 17:35:22 +0100

    test: AJout de test unitaire indexerDansSolr_Success

--- src/test/java/fr/abes/indexationsolr/services/IndexationSolrTest.java
+++ src/test/java/fr/abes/indexationsolr/services/IndexationSolrTest.java
@@ -1,12 +1,19 @@
 package fr.abes.indexationsolr.services;
 
+import static jdk.nashorn.internal.objects.Global.println;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import javax.xml.transform.TransformerException;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,6 +26,10 @@ import org.slf4j.Logger;
 @RunWith(MockitoJUnitRunner.class)
 public class IndexationSolrTest {
 
+    int iddoc = 123796;
+    String cheminXsl = "src/main/resources/xsl/sujets2solr.xsl";
+    String urlSolr = "http://denim-test.v202.abes.fr:8080/solrSujets";
+
     @Mock
     private Logger logger;
 
@@ -26,83 +37,56 @@ public class IndexationSolrTest {
     private IndexationSolr indexationSolr; // Remplace par le vrai nom de ta classe
 
     @Before
-    public void setUp() {
+    public void setUp() throws IOException {
         MockitoAnnotations.initMocks(this);
+        IndexationSolr indexer = new IndexationSolr();
+        indexer.supprimerDeSolr(iddoc, urlSolr + "/update");
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        IndexationSolr indexer = new IndexationSolr();
+        indexer.supprimerDeSolr(iddoc, urlSolr + "/update");
+    }
+
+    public String selectDocDansSolr(int idDoc, String urlSolr) throws IOException {
+        HttpURLConnection urlc = null;
+        URL url = new URL(urlSolr + "/select/?q=id:" + idDoc);
+        urlc = (HttpURLConnection) url.openConnection();
+        urlc.setRequestMethod("GET");
+        System.out.println(urlc.getContent());
+        BufferedReader in = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        return response.toString();
     }
 
     @Test
     public void indexerDansSolr_Success() throws Exception {
-        // Arrange
-        int iddoc = 123;
-        String tef = "<xml>test</xml>";
-        String cheminXsl = "src/main/resources/xsl/tef2solr.xsl";
-        String urlSolr = "http://denim.v202.abes.fr:8080/solrSujets/update";
-
+        // Indexe dans Solr
         IndexationSolr indexer = new IndexationSolr();
-
-        // Act
-        //boolean result = indexer.indexerDansSolr(iddoc, tef, cheminXsl, urlSolr);
-
-        // Assert
-        //assertTrue(result);
-        //verify(indexer).transfoXSL(tef, iddoc, cheminXsl);
-
         String docOkXML = IndexationSolr.readFile("src/test/resources/ok.xml", StandardCharsets.UTF_8);
-        indexer.indexerDansSolr(123796,
+        Boolean response = indexer.indexerDansSolr(iddoc,
                 docOkXML,
                 cheminXsl,
-                urlSolr);
+                urlSolr + "/update");
+        assertTrue(response);
 
-        assertTrue();
-    }
+        // Vérifie l'indexation
+        String response2 = selectDocDansSolr(iddoc, urlSolr);
+        assertTrue(response2.contains("<result name=\"response\" numFound=\"1\" start=\"0\">"));
+        assertTrue(response2.contains("<str name=\"id\">" + iddoc + "</str>"));
 
-    @Test
-    public void indexerDansSolr_ThrowsTransformerException() throws Exception {
-        // Arrange
-        int iddoc = 123;
-        String tef = "<xml>test</xml>";
-        String cheminXsl = "/path/to/xsl";
-        String urlSolr = "http://localhost:8983/solr";
-
-        IndexationSolr spyIndexer = spy(indexationSolr);
-
-        doThrow(new TransformerException("Transformation error")).when(spyIndexer).transfoXSL(tef, iddoc, cheminXsl);
-
-        // Act & Assert
-        try {
-            spyIndexer.indexerDansSolr(iddoc, tef, cheminXsl, urlSolr);
-            fail("Expected TransformerException");
-        } catch (TransformerException e) {
-            assertEquals("Transformation error", e.getMessage());
-        }
-
-        verify(spyIndexer).transfoXSL(tef, iddoc, cheminXsl);
-        verify(spyIndexer, never()).envoieSurSolr(anyString(), anyString());
-    }
-
-    @Test
-    public void indexerDansSolr_ThrowsIOException() throws Exception {
-        // Arrange
-        int iddoc = 123;
-        String tef = "<xml>test</xml>";
-        String cheminXsl = "/path/to/xsl";
-        String urlSolr = "http://localhost:8983/solr";
-
-        String transformedXml = "<solr><doc>test</doc></solr>";
-        IndexationSolr spyIndexer = spy(indexationSolr);
-
-        doReturn(transformedXml).when(spyIndexer).transfoXSL(tef, iddoc, cheminXsl);
-        doThrow(new IOException("Solr unreachable")).when(spyIndexer).envoieSurSolr(transformedXml, urlSolr);
-
-        // Act & Assert
-        try {
-            spyIndexer.indexerDansSolr(iddoc, tef, cheminXsl, urlSolr);
-            fail("Expected IOException");
-        } catch (IOException e) {
-            assertEquals("Solr unreachable", e.getMessage());
-        }
-
-        verify(spyIndexer).transfoXSL(tef, iddoc, cheminXsl);
-        verify(spyIndexer).envoieSurSolr(transformedXml, urlSolr);
+        // Supprime et vérifie
+        indexer.supprimerDeSolr(iddoc, urlSolr + "/update");
+        String response3 = selectDocDansSolr(iddoc, urlSolr);
+        assertTrue(response3.contains("<result name=\"response\" numFound=\"0\" start=\"0\"/>"));
+        assertFalse(response3.contains("<str name=\"id\">" + iddoc + "</str>"));
     }
 }
